2086c9f31f80934143389e06b4db80e2187c7f7c
Anastasiia Salenko <batsvinova@gmail.com>
2021-11-03 17:29:02 +0200

    Added components Contacts

---
+++ src/container/Contacts/Contacts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "../../components/UI/Button/Button";
+import classes from "./Contacts.module.css";
+
+export default function Contacts() {
+  return (
+    <div className={classes.Contacts}>
+      <h1>test</h1>
+      <Button
+        type="success"
+      >test2</Button>
+    </div>
+  )
+}
