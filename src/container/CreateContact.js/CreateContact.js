dbe1834780d397d0b7304f4885d3571aa92e0604
Anastasiia Salenko <batsvinova@gmail.com>
2021-11-19 19:18:24 +0200

    Fixed error message

--- src/container/CreateContact.js/CreateContact.js
+++ src/container/CreateContact.js/CreateContact.js
@@ -10,9 +10,9 @@ const MyTextInput = ({ label, ...props }) => {
   return (
     <>
       <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
+      <input {...field} {...props} />
       {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
+        <div className={classes.error}>{meta.error}</div>
       ) : null}
     </>
   )
